a8c87f46876353d1f0133c38a28016ab11d35857
Robert Laws <roblaws@me.com>
2018-02-13 19:31:58 +0300

    add validations, display error list, and empty toppings list

--- app/models/ice_cream.rb
+++ app/models/ice_cream.rb
@@ -2,4 +2,7 @@ class IceCream < ApplicationRecord
   has_many :ice_cream_toppings
   has_many :toppings, through: :ice_cream_toppings
   accepts_nested_attributes_for :toppings
+
+  validates :receipient, presence: true
+  validates :flavor, presence: true
 end
